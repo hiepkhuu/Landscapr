637cfde0c83b96e7a04be678bfcf8170bb979f69
hiep <hiephkhuu@gmail.com>
2021-06-17 11:22:15 -0700

    finsihed testing everything in auth.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,21 +9,6 @@ router.post ('/test', function(req, res){
 
 
 
-// TEST FOR COOKIE SUCCEESS!!GET /api/set-token-cookie
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      },
-    })
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
-
-
 
 module.exports = router;
 
@@ -37,3 +22,42 @@ module.exports = router;
 //   },
 //   body: JSON.stringify({hello: 'world'})
 // }).then(res => res.json()).then(data => console.log(data))
+
+
+
+
+// TEST FOR COOKIE SUCCEESS!!GET /api/set-token-cookie
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+//TEST FOR RESOTRE USER MIDDLEWARE
+// GET /api/restore-user
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+//TEST FOR REQUIRE AUTH
+// GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
