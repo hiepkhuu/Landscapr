0ddfbdb6b479005f68e8856120fe94c72065bb73
hiep <hiephkhuu@gmail.com>
2021-06-17 10:46:29 -0700

    added login and quth on models user.js

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');//using bcrypt for our pw validation
+
 'use strict';
 const { Validator } = require('sequelize');
 
@@ -47,6 +49,46 @@ module.exports = (sequelize, DataTypes) => {
       },
     },
   });
+
+  //*Define an Instance method for API LOGOUT/LOGINS (auth) */
+  User.prototype.toSafeObject = function() { // remember, this cannot be an arrow function - return {} with only User instacne info, save to save to JWT
+    const { id, username, email } = this; // context will be the User instance
+    return { id, username, email };
+  };
+
+  User.prototype.validatePassword = function (password) {//check if password is true
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+   };
+
+   User.getCurrentUserById = async function (id) {//use currentUser to return User with taht id
+    return await User.scope('currentUser').findByPk(id);
+   };
+
+   //**Define Static method accepting an object with credential and pw key */
+   User.login = async function ({ credential, password }) {//accept an obj with credential and pw
+    const { Op } = require('sequelize');
+    const user = await User.scope('loginUser').findOne({//search for user with the credential(username or email)
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
+        },
+      },
+    });
+    if (user && user.validatePassword(password)) {//if username is found + validate pw helps, return user's scope
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+  };
+  
+  User.signup = async function ({ username, email, password }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword,
+    });
+    return await User.scope('currentUser').findByPk(user.id);
+  };
 
   User.associate = function(models) {
     // associations can be defined here
