ed3098a47fd12be3761e597fe0a42291c459fa91
hiep <hiephkhuu@gmail.com>
2021-06-17 11:18:24 -0700

    phase 3 completed

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,25 @@ router.post ('/test', function(req, res){
   res.json({requestBody: req.body})
 })
 
+
+
+
+// TEST FOR COOKIE SUCCEESS!!GET /api/set-token-cookie
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+
+
 module.exports = router;
 
 
