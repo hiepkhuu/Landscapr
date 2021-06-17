381a0543b87445123bddcc91b29a9ad7c405c957
hiep <hiephkhuu@gmail.com>
2021-06-16 22:56:15 -0700

    created error handlers, ready to test soon

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,42 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(routes);// Connect all the routes
 
+//IMPORT ERROR HANDLERS//
+const { ValidationError } = require('sequelize');
+
+//***ERROR HANDLERS */
+//resource-not-found---Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
+
 // Security Middleware
 if (!isProduction) {
   // enable cors only in development
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const csrf = require('csurf');// not sur eif we need this for csrfToken
+const csrfProtection = csrf({cookie: true})
 
 /** CONNECT ROUTERS TO THIS FILE */
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN');//, req.csrfToken() - commented out as it broke the code
+router.get('/hello/world', csrfProtection, function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());//, req.csrfToken() - commented out as it broke the code
   res.send('Hello World!');
 });
 
