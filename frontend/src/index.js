ab2cf28753c594d016e840d337650a054dbc9d29
hiep <hiephkhuu@gmail.com>
2021-06-17 15:27:58 -0700

    finsihed setting up redux provider and browserrouter inside store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
+
+
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
+import configureStore from './store'
+
+const store = configureStore();
+
+if(process.env.NODE_ENV !== 'production'){//so store won't be exposed in production environment
+  window.store = store;
+}
+
+
+//app is wraped in provider
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+//root is rendered
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
