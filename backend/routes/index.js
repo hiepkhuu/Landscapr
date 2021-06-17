9064ff53ff518075763224f2f13d8f0af99bca00
hiep <hiephkhuu@gmail.com>
2021-06-16 22:20:19 -0700

    added routers and api router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+// const csrf = require('csurf');// not sur eif we need this for csrfToken
+
+/** CONNECT ROUTERS TO THIS FILE */
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.cookie('XSRF-TOKEN');//, req.csrfToken() - commented out as it broke the code
   res.send('Hello World!');
 });
 
