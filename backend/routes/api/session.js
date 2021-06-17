1549ea97304c3d523dae1c74f06c13a1acd7f278
hiep <hiephkhuu@gmail.com>
2021-06-17 11:58:52 -0700

    finished testing login routes

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,4 +1,57 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler');//wrap asynchronous route handlers and custom middlewares.
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+
 const router = express.Router();
 
+//this is for USERS-LOGIN
+router.post(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  }),
+);
+
+
 module.exports = router;
+
+//test if login actually works
+// fetch('/api/session', {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN":`nzr1rDdO-0Qx3y9xIxS3PyD8lvWw1kTHXQUc`
+//   },
+//   body: JSON.stringify({ credential: 'Demo-lition', password: 'password' })
+// }).then(res => res.json()).then(data => console.log(data));
+
+
+
+//test if login email works
+// fetch('/api/session', {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": `nzr1rDdO-0Qx3y9xIxS3PyD8lvWw1kTHXQUc`
+//   },
+//   body: JSON.stringify({ credential: 'demo@user.io', password: 'password' })
+// }).then(res => res.json()).then(data => console.log(data));
